9738ed48a7f337abf786e926f9dfac149d04ecfa
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2024-08-09 23:11:30 -0500

    Reverted to CLI app, planning to use ratatui to flesh it out Added forecast option(short and detailed) with option to change time periods

--- src/main.rs
+++ src/main.rs
@@ -1,139 +1,38 @@
-mod weather_request;
 
 use reqwest::Client;
 use tokio;
 use std::io::{self, Write};
-use std::ops::Deref;
-use std::sync::{Arc, Mutex};
-use eframe::egui::{CentralPanel, CtxRef};
-use eframe::epi::{App, Frame};
-use eframe::{egui, NativeOptions, run_native};
-use tokio::io::AsyncBufReadExt;
-use tokio::runtime::Runtime;
-use tokio::sync::mpsc;
-use crate::egui::mutex::MutexGuard;
-use crate::weather_request::WeatherRequest;
+use weather_forecasting::weather_request::WeatherRequest;
 
 
 #[tokio::main]
 async fn main() {
-    /*
+
     let mut prompt: bool = true;
     let mut input:String = String::new();
     let mut split_words: Vec<&str> = input.trim().split_whitespace().collect();
-    let mut forecaster = WeatherRequest {
-        grid_x : "".parse().unwrap(),
-        grid_y : "".parse().unwrap(),
-        grid_id : "".parse().unwrap()
-    };
+    let mut forecaster = WeatherRequest::new();
     while prompt {
         input.clear();
         io::stdin().read_line(&mut input).expect("Failed to read line");
         split_words = input.trim().split_whitespace().collect();
         if split_words.len() < 1 { continue }
         match split_words[0] {
-            "location" => {forecaster.get_location(split_words[1], split_words[2]).await}
+            "update" => {forecaster.update_location(split_words[1], split_words[2]).await}
+            "location" => {forecaster.get_location().await}
             "day" => {forecaster.get_is_day().await}
             "temperature" => {forecaster.get_temperature().await}
             "json" => {forecaster.get_json().await}
-            "exit" => {prompt =false}
+            "exit" => {prompt = false}
+            "short_forecast" => {forecaster.get_short_forecast().await}
+            "detailed_forecast" => {forecaster.get_detailed_forecast().await}
+            "increment" => {forecaster.increment_time_period()}
+            "decrement" => {forecaster.decrement_time_period()}
             &_ => {}
         }
     }
-    */
-
-
-    let app = Weather::new();
-    let win_options = NativeOptions::default();
-    run_native(Box::new(app), win_options);
-}
-
-async fn get_text() -> String{
-    "check".to_string()
-}
-
-async fn async_operation() -> String {
-    tokio::time::sleep(std::time::Duration::from_secs(2)).await;
-    "Async operation result".to_string()
-}
-
-async fn get_temperature(lat : &str, lon : &str) -> String {
-    println!("lat : {}, lon : {}", lat, lon);
-    let mut temp = WeatherRequest {
-        grid_x : "".to_string(),
-        grid_y : "".to_string(),
-        grid_id : "".to_string()
-    };
-    temp.update_location(lat, lon).await;
-    temp.get_temperature().await
-}
-
-struct Weather {
-    async_runtime: Runtime,
-    sender: mpsc::Sender<String>,
-    receiver: mpsc::Receiver<String>,
-    message: Arc<Mutex<String>>,
-    temperature: Arc<Mutex<String>>,
-    latitude_input: String,
-    longitude_input : String
-}
-
-impl Weather{
-    fn new() -> Self {
-        let (sender, receiver) = mpsc::channel(100);
-        let message = Arc::new(Mutex::new(String::new()));
-        let temperature = Arc::new(Mutex::new(String::new()));
-
-        let runtime = Runtime::new().unwrap();
-
-        Self {
-            async_runtime: runtime,
-            sender,
-            receiver,
-            message,
-            temperature,
-            latitude_input : "".to_string(),
-            longitude_input : "".to_string()
-        }
-    }
-
-
 }
 
 
-impl App for Weather{
-    fn update(&mut self, ctx: &CtxRef, frame: &mut Frame<'_>) {
-        if let Ok(result) = self.receiver.try_recv() {
-            let mut msg = self.temperature.lock().unwrap();
-            *msg = result;
-        }
 
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Enter latitude and longitude to get temperature");
-            //ui.label("This is a simple eframe application.");
-            let msg = self.temperature.lock().unwrap();
-            ui.label("Temperature : ".to_owned() + msg.as_str()); // Display the message
-            ui.text_edit_singleline(&mut self.latitude_input);
-            ui.text_edit_singleline(&mut self.longitude_input);
-            if ui.button("Update Message").clicked() {
-                let sender = self.sender.clone();
-                let message_clone = Arc::clone(&self.temperature);
-                let longitude_input = self.longitude_input.clone();
-                let latitude_input = self.latitude_input.clone();
-                self.async_runtime.spawn(async move {
-                    let result = get_temperature(&latitude_input, &longitude_input).await;
-                    sender.send(result).await.unwrap();
-                    let mut msg = message_clone.lock().unwrap();
-                    *msg = "Async operation completed!".to_string();
-                });
-            }
-        });
-
-        //println!("{}", self.message.lock().unwrap());
-    }
-
-    fn name(&self) -> &str {
-        "Weather"
-    }
-}
--- src/weather_request.rs
+++ src/weather_request.rs
@@ -1,46 +1,88 @@
 use reqwest::Client;
 
 pub struct WeatherRequest {
-    pub(crate) grid_x: String,
-    pub(crate) grid_y: String,
-    pub(crate) grid_id: String,
+    grid_x: String,
+    grid_y: String,
+    grid_id: String,
+    client: Client,
+    time_period : usize,
 }
 
 impl WeatherRequest{
-    pub(crate) async fn update_location(&mut self, lat : &str, lon : &str) -> bool{
-        let client = Client::new();
-        let grid_string = client.get("https://api.weather.gov/points/".to_owned()+lat+"," + lon).header("User-Agent", "request").send().await.expect("panic").text().await.unwrap();
+
+    pub fn new() -> Self{
+        Self {
+            grid_x : "".parse().unwrap(),
+            grid_y : "".parse().unwrap(),
+            grid_id : "".parse().unwrap(),
+            client : Client::new(),
+            time_period : 0
+        }
+    }
+
+    pub async fn update_location(&mut self, lat : &str, lon : &str){
+        let grid_string = self.client.get("https://api.weather.gov/points/".to_owned()+lat+"," + lon).header("User-Agent", "request").send().await.expect("panic").text().await.unwrap();
         let grid = (&json::parse(&*grid_string).unwrap()["properties"]["gridX"], &json::parse(&*grid_string).unwrap()["properties"]["gridY"], &json::parse(&*grid_string).unwrap()["properties"]["gridId"]);
-        println!("grid X : {}", grid.0);
-        println!("grid Y : {}", grid.1);
-        println!("grid id : {}", grid.2);
         self.grid_x = grid.0.to_string();
         self.grid_y = grid.1.to_string();
         self.grid_id = grid.2.to_string();
-        true
+
     }
 
+    pub async fn get_location(&self) {
+        println!("grid X : {}", self.grid_x);
+        println!("grid Y : {}", self.grid_y);
+        println!("grid id : {}", self.grid_id);
+    }
 
-
-    pub(crate) async fn get_is_day(&self){
-        let client = Client::new();
-        let response = client.get("https://api.weather.gov/gridpoints/".to_owned()+ &*self.grid_id +"/"+&*self.grid_x+","+&*self.grid_y+"/forecast").header("User-Agent", "reqwest").send().await.unwrap().text().await.unwrap();
+    pub async fn get_is_day(&self){
+        let response = self.client.get("https://api.weather.gov/gridpoints/".to_owned()+ &*self.grid_id +"/"+&*self.grid_x+","+&*self.grid_y+"/forecast").header("User-Agent", "reqwest").send().await.unwrap().text().await.unwrap();
         let response_json = json::parse(&*response).unwrap();
-        println!("{}", response_json["properties"]["periods"][0]["isDaytime"]);
+        println!("{}", response);
+        println!("{}", response_json["properties"]["periods"][self.time_period]["isDaytime"]);
     }
 
-    pub(crate) async fn get_temperature(&self) -> String{
-        let client = Client::new();
-        let response = client.get("https://api.weather.gov/gridpoints/".to_owned()+ &*self.grid_id +"/"+&*self.grid_x+","+&*self.grid_y+"/forecast").header("User-Agent", "reqwest").send().await.unwrap().text().await.unwrap();
+    pub async fn get_temperature(&self){
+        let response = self.client.get("https://api.weather.gov/gridpoints/".to_owned()+ &*self.grid_id +"/"+&*self.grid_x+","+&*self.grid_y+"/forecast").header("User-Agent", "reqwest").send().await.unwrap().text().await.unwrap();
         let response_json = json::parse(&*response).unwrap();
         //println!("{}", response_json["properties"]["periods"][0]["temperature"]);
-        response_json["properties"]["periods"][0]["temperature"].to_string()
+        println!("{}", response_json["properties"]["periods"][self.time_period]["temperature"].to_string());
     }
 
-    pub(crate) async fn get_json(&self){
-        let client = Client::new();
-        let response = client.get("https://api.weather.gov/gridpoints/".to_owned()+ &*self.grid_id +"/"+&*self.grid_x+","+&*self.grid_y+"/forecast").header("User-Agent", "reqwest").send().await.unwrap().text().await.unwrap();
+    pub async fn get_json(&self){
+        let response = self.client.get("https://api.weather.gov/gridpoints/".to_owned()+ &*self.grid_id +"/"+&*self.grid_x+","+&*self.grid_y+"/forecast").header("User-Agent", "reqwest").send().await.unwrap().text().await.unwrap();
         let response_json = json::parse(&*response).unwrap();
         println!("{}", response_json["properties"]["periods"]);
+    }
+
+    pub async fn get_short_forecast(&self){
+        let response = self.client.get("https://api.weather.gov/gridpoints/".to_owned()+ &*self.grid_id +"/"+&*self.grid_x+","+&*self.grid_y+"/forecast").header("User-Agent", "reqwest").send().await.unwrap().text().await.unwrap();
+        let response_json = json::parse(&*response).unwrap();
+        println!("{}", response_json["properties"]["periods"][self.time_period]["shortForecast"]);
+    }
+
+    pub async fn get_detailed_forecast(&self){
+        let response = self.client.get("https://api.weather.gov/gridpoints/".to_owned()+ &*self.grid_id +"/"+&*self.grid_x+","+&*self.grid_y+"/forecast").header("User-Agent", "reqwest").send().await.unwrap().text().await.unwrap();
+        let response_json = json::parse(&*response).unwrap();
+        println!("{}", response_json["properties"]["periods"][self.time_period]["detailedForecast"]);
+    }
+
+    pub fn increment_time_period(&mut self){
+        if self.time_period < 13 {
+            self.time_period += 1;
+            println!("Time period is now at {}", self.time_period);
+        } else {
+            println!("Time period unable to be incremented");
+        }
+
+    }
+
+    pub fn decrement_time_period(&mut self){
+        if self.time_period > 0 {
+            self.time_period -= 1;
+            println!("Time period is now at {}", self.time_period);
+        } else {
+            println!("Time period unable to be decremented");
+        }
     }
 }
